59e3424d467f2115003fe78921dfaa4b1687aa3d
Vitalmet <vuyko87@bk.ru>
2026-05-02 09:55:10 +0300

    Созал обработчики, шаблоны, задеплоил.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,19 +1,34 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 #загружаем переменные окружения из .env
 load_dotenv()
 
-app = Flask(__name__)
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+app = Flask(__name__,template_folder=template_dir)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-secret-key-for-dev')
 
 
 
 @app.route("/")
 def index():
-   return "Анализатор страниц приветствует тебя!"
+   return render_template('index.html')
 
-@app.route("/analyze")
-def analyze():
-   return {"статус": "готов", "сообщене": "Анализатор страниц работает!"}
+@app.route("/chek", methods=['POST'])
+def chek_url():
+   url = request.form.get('url', '').strip()
+
+   if not url:
+      flash('URL не может быть пустым', 'danger')
+      return redirect(url_for('index'))
+
+   if not url.startswith(('http://', 'https://')):
+      flash('URL должен начинаться с http:// или https://', 'danger')
+      return redirect(url_for('index'))
+
+   flash(f'Анализ страницы {url} начат', 'success')
+   return redirect(url_for('index'))
+
+if __name__ == "__main__":
+   app.run(debug=True)
